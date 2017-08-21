13bf707f33bf06df01c35d67589e43808c1b6989
DeviPotato <jschrip@gmail.com>
2017-08-21 12:11:21 -0700

    More complete Shooter data

--- weapons.js
+++ weapons.js
@@ -3,356 +3,495 @@ function filter_available(weapon) {
 }
 
 angular.module('splatApp').weapons = function ($scope) {
-$scope.weaponSets = [{
+$scope.weaponSets = [
+  {
   type: "Shooters",
   weapons: [
     {
+      image: "img/weapons/Wst_Shooter_First_00.png",
       name: "Splattershot Jr.",
       shotUnit: "shots",
-      image: "img/weapons/Wst_Shooter_First_00.png",
       sub: "Splat Bomb",
       special: "Ink Armor",
-      specialCost: 180,
       level: 1,
+      specialCost: 180,
       inkSaver: "Middle",
+      inkPerShot: 0.5,
       speedLevel: "High",
-      inkPerShot: 0.5
+      price: 0,
+      stats: {
+        "Range": 35,
+        "Fire Rate": 75,
+        "Damage": 22
+      }
     },
     {
+      image: "img/weapons/Wst_Shooter_Normal_H.png",
+      name: "Hero Shot Replica",
+      shotUnit: "shots",
+      sub: "Burst Bomb",
+      special: "Splashdown",
+      level: 2,
+      specialCost: 180,
+      inkSaver: "Middle",
+      inkPerShot: 1,
+      speedLevel: "Middle",
+      price: 1500,
+      stats: {
+        "Range": 50,
+        "Fire Rate": 60,
+        "Damage": 45
+      }
+    },
+    {
+      image: "img/weapons/Wst_Shooter_Normal_00.png",
       name: "Splattershot",
       shotUnit: "shots",
-      image: "img/weapons/Wst_Shooter_Normal_00.png",
       sub: "Burst Bomb",
       special: "Splashdown",
-      specialCost: 180,
       level: 2,
-      inkSaver: "Middle",
-      speedLevel: "Middle",
-      inkPerShot: 1
-    },
-    {
-      name: "Splat Dualies",
-      shotUnit: "shots",
-      image: "img/weapons/Wst_Twins_Normal_00.png",
-      sub: "Burst Bomb",
-      special: "Tenta Missiles",
       specialCost: 180,
-      level: 4,
       inkSaver: "Middle",
+      inkPerShot: 1,
       speedLevel: "Middle",
-      inkPerShot: 0.8
+      price: 900,
+      stats: {
+        "Range": 50,
+        "Fire Rate": 60,
+        "Damage": 45
+      }
     },
     {
+      image: "img/weapons/Wst_Shooter_Normal_01.png",
       name: "Tentatek Splattershot",
       shotUnit: "shots",
-      image: "img/weapons/Wst_Shooter_Normal_01.png",
       sub: "Splat Bomb",
       special: "Inkjet",
-      specialCost: 190,
       level: 4,
+      specialCost: 210,
       inkSaver: "Middle",
+      inkPerShot: 1,
       speedLevel: "Middle",
-      inkPerShot: 1
+      price: 2100,
+      stats: {
+        "Range": 50,
+        "Fire Rate": 60,
+        "Damage": 45
+      }
     },
     {
-      name: "Aerospray MG",
-      shotUnit: "shots",
-      image: "img/weapons/Wst_Shooter_Blaze_00.png",
-      sub: "Suction Bomb",
-      special: "Curling-Bomb Launcher",
-      specialCost: 170,
-      level: 6,
-      inkSaver: "Middle",
-      speedLevel: "High",
-      inkPerShot: 0.55
-    },
-    {
-      name: "N-ZAP '85",
-      shotUnit: "shots",
-      image: "img/weapons/Wst_Shooter_QuickMiddle_00.png",
-      sub: "Suction Bomb",
-      special: "Ink Armor",
-      specialCost: 200,
-      level: 9,
-      inkSaver: "Middle",
-      speedLevel: "Middle",
-      inkPerShot: 0.8
-    },
-    {
-      name: "Splattershot Pro",
-      shotUnit: "shots",
-      image: "img/weapons/Wst_Shooter_Expert_00.png",
-      sub: "Point Sensor",
-      special: "Ink Storm",
-      specialCost: 180,
-      level: 10,
-      inkSaver: "High",
-      speedLevel: "Middle",
-      inkPerShot: 2
-    },
-    {
-      name: "Enperry Splat Dualies",
-      shotUnit: "shots",
-      image: "img/weapons/Wst_Twins_Normal_01.png",
-      sub: "Curling Bomb",
-      special: "Inkjet",
-      specialCost: 180,
-      level: 11,
-      inkSaver: "Middle",
-      speedLevel: "Middle",
-      inkPerShot: 0.8
-    },
-    {
-      name: "Dualie Squelchers",
-      shotUnit: "shots",
-      image: "img/weapons/Wst_Twins_Dual_00.png",
-      sub: "Point Sensor",
-      special: "Tenta Missiles",
-      specialCost: 180,
-      level: 12,
-      inkSaver: "Middle",
-      speedLevel: "Middle",
-      inkPerShot: 1
-    },
-    {
-      name: ".52 Gal",
-      shotUnit: "shots",
-      image: "img/weapons/Wst_Shooter_Gravity_00.png",
-      sub: "Point Sensor",
-      special: "Baller",
-      specialCost: 180,
-      level: 14,
-      inkSaver: "Middle",
-      speedLevel: "Middle",
-      inkPerShot: 1.3
-    },
-    {
-      name: "Jet Squelcher",
-      shotUnit: "shots",
-      image: "img/weapons/Wst_Shooter_Long_00.png",
-      sub: "Toxic Mist",
-      special: "Tenta Missiles",
-      specialCost: 180,
-      level: 17,
-      inkSaver: "Middle",
-      speedLevel: "Middle",
-      inkPerShot: 1.6
-    },
-    {
-      name: "L-3 Nozzlenose",
-      shotUnit: "shots",
-      image: "img/weapons/Wst_Shooter_TripleQuick_00.png",
-      sub: "Curling Bomb",
-      special: "Baller",
-      specialCost: 180,
-      level: 18,
-      inkSaver: "Middle",
-      speedLevel: "Middle",
-      inkPerShot: 1
-    },
-    {
-      name: ".96 Gal",
-      shotUnit: "shots",
-      image: "img/weapons/Wst_Shooter_Heavy_00.png",
-      sub: "Sprinkler",
-      special: "Ink Armor",
-      specialCost: 180,
-      level: 21,
-      inkSaver: "Middle",
-      speedLevel: "Middle",
-      inkPerShot: 2.5
-    },
-    {
-      name: "Splash-o-matic",
-      shotUnit: "shots",
-      image: "img/weapons/Wst_Shooter_Precision_00.png",
-      sub: "Toxic Mist",
-      special: "Inkjet",
-      specialCost: 180,
-      level: 25,
-      inkSaver: "Middle",
-      speedLevel: "High",
-      inkPerShot: 0.8
-    },
-    {
-      name: "Dapple Dualies",
-      shotUnit: "shots",
-      image: "img/weapons/Wst_Twins_Short_00.png",
-      sub: "Squid Beakon",
-      special: "Suction-Bomb Launcher",
-      specialCost: 180,
-      level: 26,
-      inkSaver: "Middle",
-      speedLevel: "High",
-      inkPerShot: 0.9
-    },
-    {
-      name: "Aerospray RG",
-      shotUnit: "shots",
-      image: "img/weapons/Wst_Shooter_Blaze_01.png",
-      sub: "Sprinkler",
-      special: "Baller",
-      specialCost: 180,
-      level: 28,
-      inkSaver: "Middle",
-      speedLevel: "High",
-      inkPerShot: 0.55
-    },
-    {
-      name: "H-3 Nozzlenose",
-      shotUnit: "shots",
-      image: "img/weapons/Wst_Shooter_TripleMiddle_01.png",
-      sub: "Point Sensor",
-      special: "Tenta Missiles",
-      specialCost: 180,
-      level: 29,
-      inkSaver: "High",
-      speedLevel: "Middle",
-      inkPerShot: 1.8
-    },
-    {
+      image: "img/weapons/Wst_Shooter_First_01.png",
       name: "Custom Splattershot Jr.",
       shotUnit: "shots",
-      image: "img/weapons/Wst_Shooter_First_01.png",
       sub: "Autobomb",
       special: "Ink Storm",
-      specialCost: 180,
       level: 4,
+      specialCost: 180,
       inkSaver: "Middle",
-      speedLevel: "High",
       inkPerShot: 0.5,
-      unreleased: true
+      speedLevel: "High",
+      price: 1900,
+      stats: {
+        "Range": 35,
+        "Fire Rate": 75,
+        "Damage": 22
+      }
     },
     {
+      image: "img/weapons/Wst_Shooter_Blaze_00.png",
+      name: "Aerospray MG",
+      shotUnit: "shots",
+      sub: "Suction Bomb",
+      special: "Curling-Bomb Launcher",
+      level: 6,
+      specialCost: 170,
+      inkSaver: "Middle",
+      inkPerShot: 0.55,
+      speedLevel: "High",
+      price: 4900,
+      stats: {
+        "Range": 35,
+        "Fire Rate": 90,
+        "Damage": 10
+      }
+    },
+    {
+      image: "img/weapons/Wst_Shooter_QuickMiddle_00.png",
+      name: "N-ZAP '85",
+      shotUnit: "shots",
+      sub: "Suction Bomb",
+      special: "Ink Armor",
+      level: 9,
+      specialCost: 200,
+      inkSaver: "Middle",
+      inkPerShot: 0.8,
+      speedLevel: "Middle",
+      price: 7100,
+      stats: {
+        "Range": 50,
+        "Fire Rate": 75,
+        "Damage": 22
+      }
+    },
+    {
+      image: "img/weapons/Wst_Shooter_Short_00.png",
+      name: "Sploosh-o-matic",
+      shotUnit: "shots",
+      sub: "Curling Bomb",
+      special: "Splashdown",
+      level: 10,
+      specialCost: 180,
+      inkSaver: "Middle",
+      inkPerShot: 0.8,
+      speedLevel: "High",
+      price: 9700,
+      stats: {
+        "Range": 12,
+        "Fire Rate": 75,
+        "Damage": 52
+      }
+    },
+    {
+      image: "img/weapons/Wst_Shooter_Expert_00.png",
+      name: "Splattershot Pro",
+      shotUnit: "shots",
+      sub: "Point Sensor",
+      special: "Ink Storm",
+      level: 10,
+      specialCost: 180,
+      inkSaver: "High",
+      inkPerShot: 2,
+      speedLevel: "Middle",
+      price: 13800,
+      stats: {
+        "Range": 70,
+        "Fire Rate": 30,
+        "Damage": 60
+      }
+    },
+    {
+      image: "img/weapons/Wst_Shooter_QuickMiddle_01.png",
       name: "N-ZAP '89",
       shotUnit: "shots",
-      image: "img/weapons/Wst_Shooter_QuickMiddle_01.png",
       sub: "Autobomb",
       special: "Tenta Missiles",
-      specialCost: 180,
       level: 11,
+      specialCost: 180,
       inkSaver: "Middle",
-      speedLevel: "Middle",
       inkPerShot: 0.8,
-      unreleased: true
-    },
-    {
-      name: "Custom Dualie Squelchers",
-      shotUnit: "shots",
-      image: "img/weapons/Wst_Twins_Dual_01.png",
-      sub: "Splat Bomb",
-      special: "Ink Armor",
-      specialCost: 190,
-      level: 16,
-      inkSaver: "Middle",
       speedLevel: "Middle",
-      inkPerShot: 1,
-      unreleased: true
+      price: 8800,
+      stats: {
+        "Range": 50,
+        "Fire Rate": 75,
+        "Damage": 22
+      }
     },
     {
-      name: "Forge Splattershot Pro",
+      image: "img/weapons/Wst_Shooter_Gravity_00.png",
+      name: ".52 Gal",
       shotUnit: "shots",
-      image: "img/weapons/Wst_Shooter_Expert_01.png",
-      sub: "Suction Bomb",
-      special: "Bubble Blower",
+      sub: "Point Sensor",
+      special: "Baller",
+      level: 14,
       specialCost: 180,
-      level: 20,
-      inkSaver: "High",
-      speedLevel: "Middle",
-      inkPerShot: 2,
-      unreleased: true
-    },
-    {
-      name: ".52 Gal Deco",
-      shotUnit: "shots",
-      image: "img/weapons/Wst_Shooter_Gravity_01.png",
-      sub: "Curling Bomb",
-      special: "Sting Ray",
-      specialCost: 180,
-      level: 22,
       inkSaver: "Middle",
       inkPerShot: 1.3,
-      unreleased: true
-    },
-    {
-      name: "L-3 Nozzlenose D",
-      shotUnit: "shots",
-      image: "img/weapons/Wst_Shooter_TripleQuick_01.png",
-      sub: "Burst Bomb",
-      special: "Inkjet",
-      specialCost: 180,
-      level: 23,
-      inkSaver: "Middle",
       speedLevel: "Middle",
-      inkPerShot: 1,
-      unreleased: true
+      price: 9500,
+      stats: {
+        "Range": 50,
+        "Fire Rate": 25,
+        "Damage": 75
+      }
     },
     {
-      name: "Custom Jet Squelcher",
+      image: "img/weapons/Wst_Shooter_Flash_00.png",
+      name: "Squeezer",
       shotUnit: "shots",
-      image: "img/weapons/Wst_Shooter_Long_01.png",
-      sub: "Burst Bomb",
-      special: "Sting Ray",
-      specialCost: 180,
-      level: 27,
-      inkSaver: "Middle",
-      speedLevel: "Middle",
-      inkPerShot: 1.6,
-      unreleased: true
-    },
-    {
-      name: "Neo Splash-o-matic",
-      shotUnit: "shots",
-      image: "img/weapons/Wst_Shooter_Precision_01.png",
-      sub: "Burst Bomb",
-      special: "Suction-Bomb Launcher",
-      specialCost: 180,
-      level: 27,
-      inkSaver: "Middle",
-      speedLevel: "High",
-      inkPerShot: 0.8,
-      unreleased: true
-    },
-    {
-      name: ".96 Gal Deco",
-      shotUnit: "shots",
-      image: "img/weapons/Wst_Shooter_Heavy_01.png",
       sub: "Splash Wall",
-      special: "Splashdown",
+      special: "Sting Ray",
+      level: 16,
       specialCost: 180,
-      level: 26,
       inkSaver: "Middle",
       speedLevel: "Middle",
-      inkPerShot: 2.5,
-      unreleased: true
+      price: 9400,
+      stats: {
+        "Range": 70,
+        "Fire Rate": 45,
+        "Damage": 52
+      }
     },
     {
-      name: "Dapple Dualies Nouveau",
+      image: "img/weapons/Wst_Shooter_Long_00.png",
+      name: "Jet Squelcher",
       shotUnit: "shots",
-      image: "img/weapons/Wst_Twins_Short_01.png",
       sub: "Toxic Mist",
-      special: "Ink Storm",
+      special: "Tenta Missiles",
+      level: 17,
       specialCost: 180,
-      level: 29,
       inkSaver: "Middle",
-      speedLevel: "High",
-      inkPerShot: 0.9,
-      unreleased: true
+      inkPerShot: 1.6,
+      speedLevel: "Middle",
+      price: 11300,
+      stats: {
+        "Range": 82,
+        "Fire Rate": 30,
+        "Damage": 35
+      }
     },
     {
-      name: "H-3 Nozzlenose D",
+      image: "img/weapons/Wst_Shooter_TripleQuick_00.png",
+      name: "L-3 Nozzlenose",
       shotUnit: "shots",
-      image: "img/weapons/Wst_Shooter_TripleMiddle_01.png",
+      sub: "Curling Bomb",
+      special: "Baller",
+      level: 18,
+      specialCost: 180,
+      inkSaver: "Middle",
+      inkPerShot: 1,
+      speedLevel: "Middle",
+      price: 10400,
+      stats: {
+        "Range": 62,
+        "Fire Rate": 65,
+        "Damage": 25
+      }
+    },
+    {
+      image: "img/weapons/Wst_Shooter_Short_01.png",
+      name: "Neo Sploosh-o-matic",
+      shotUnit: "shots",
+      sub: "Squid Beakon",
+      special: "Tenta Missiles",
+      level: 18,
+      specialCost: 170,
+      inkSaver: "Middle",
+      inkPerShot: 0.8,
+      speedLevel: "High",
+      price: 12200,
+      stats: {
+        "Range": 12,
+        "Fire Rate": 75,
+        "Damage": 52
+      }
+    },
+    {
+      image: "img/weapons/Wst_Shooter_Expert_01.png",
+      name: "Forge Splattershot Pro",
+      shotUnit: "shots",
+      sub: "Suction Bomb",
+      special: "Bubble Blower",
+      level: 20,
+      specialCost: 180,
+      inkSaver: "High",
+      inkPerShot: 2,
+      speedLevel: "Middle",
+      price: 20800,
+      stats: {
+        "Range": 70,
+        "Fire Rate": 30,
+        "Damage": 60
+      }
+    },
+    {
+      image: "img/weapons/Wst_Shooter_Heavy_00.png",
+      name: ".96 Gal",
+      shotUnit: "shots",
       sub: "Sprinkler",
       special: "Ink Armor",
-      specialCost: 190,
-      level: 30,
-      inkSaver: "High",
+      level: 21,
+      specialCost: 210,
+      inkSaver: "Middle",
+      inkPerShot: 2.5,
       speedLevel: "Middle",
+      price: 12600,
+      stats: {
+        "Range": 70,
+        "Fire Rate": 10,
+        "Damage": 75
+      }
+    },
+    {
+      image: "img/weapons/Wst_Shooter_Gravity_01.png",
+      name: ".52 Gal Deco",
+      shotUnit: "shots",
+      sub: "Curling Bomb",
+      special: "Sting Ray",
+      level: 22,
+      specialCost: 180,
+      inkSaver: "Middle",
+      inkPerShot: 1.3,
+      speedLevel: "Middle",
+      price: 12700,
+      stats: {
+        "Range": 50,
+        "Fire Rate": 25,
+        "Damage": 75
+      }
+    },
+    {
+      image: "img/weapons/Wst_Shooter_TripleQuick_01.png",
+      name: "L-3 Nozzlenose D",
+      shotUnit: "shots",
+      sub: "Burst Bomb",
+      special: "Inkjet",
+      level: 23,
+      specialCost: 180,
+      inkSaver: "Middle",
+      inkPerShot: 1,
+      speedLevel: "Middle",
+      price: 12500,
+      stats: {
+        "Range": 62,
+        "Fire Rate": 65,
+        "Damage": 25
+      }
+    },
+    {
+      image: "img/weapons/Wst_Shooter_Precision_00.png",
+      name: "Splash-o-matic",
+      shotUnit: "shots",
+      sub: "Toxic Mist",
+      special: "Inkjet",
+      level: 25,
+      specialCost: 180,
+      inkSaver: "Middle",
+      inkPerShot: 0.8,
+      speedLevel: "High",
+      price: 11200,
+      stats: {
+        "Range": 35,
+        "Fire Rate": 75,
+        "Damage": 22
+      }
+    },
+    {
+      image: "img/weapons/Wst_Shooter_Flash_01.png",
+      name: "Foil Squeezer",
+      shotUnit: "shots",
+      sub: "Splat Bomb",
+      special: "Bubble Blower",
+      level: 25,
+      specialCost: 180,
+      inkSaver: "Middle",
+      speedLevel: "Middle",
+      price: 10900,
+      stats: {
+        "Range": 70,
+        "Fire Rate": 45,
+        "Damage": 52
+      }
+    },
+    {
+      image: "img/weapons/Wst_Shooter_Heavy_01.png",
+      name: ".96 Gal Deco",
+      shotUnit: "shots",
+      sub: "Splash Wall",
+      special: "Splashdown",
+      level: 26,
+      specialCost: 180,
+      inkSaver: "Middle",
+      inkPerShot: 2.5,
+      speedLevel: "Middle",
+      price: 16200,
+      stats: {
+        "Range": 70,
+        "Fire Rate": 10,
+        "Damage": 75
+      }
+    },
+    {
+      image: "img/weapons/Wst_Shooter_Precision_01.png",
+      name: "Neo Splash-o-matic",
+      shotUnit: "shots",
+      sub: "Burst Bomb",
+      special: "Suction-Bomb Launcher",
+      level: 27,
+      specialCost: 180,
+      inkSaver: "Middle",
+      inkPerShot: 0.8,
+      speedLevel: "High",
+      price: 16800,
+      stats: {
+        "Range": 35,
+        "Fire Rate": 75,
+        "Damage": 22
+      }
+    },
+    {
+      image: "img/weapons/Wst_Shooter_Long_01.png",
+      name: "Custom Jet Squelcher",
+      shotUnit: "shots",
+      sub: "Burst Bomb",
+      special: "Sting Ray",
+      level: 27,
+      specialCost: 180,
+      inkSaver: "Middle",
+      inkPerShot: 1.6,
+      speedLevel: "Middle",
+      price: 15900,
+      stats: {
+        "Range": 82,
+        "Fire Rate": 30,
+        "Damage": 35
+      }
+    },
+    {
+      image: "img/weapons/Wst_Shooter_Blaze_01.png",
+      name: "Aerospray RG",
+      shotUnit: "shots",
+      sub: "Sprinkler",
+      special: "Baller",
+      level: 28,
+      specialCost: 180,
+      inkSaver: "Middle",
+      inkPerShot: 0.55,
+      speedLevel: "High",
+      price: 16900,
+      stats: {
+        "Range": 35,
+        "Fire Rate": 90,
+        "Damage": 10
+      }
+    },
+    {
+      image: "img/weapons/Wst_Shooter_TripleMiddle_00.png",
+      name: "H-3 Nozzlenose",
+      shotUnit: "shots",
+      sub: "Point Sensor",
+      special: "Tenta Missiles",
+      level: 29,
+      specialCost: 180,
+      inkSaver: "High",
       inkPerShot: 1.8,
-      unreleased: true
+      speedLevel: "Middle",
+      price: 17200,
+      stats: {
+        "Range": 70,
+        "Fire Rate": 30,
+        "Damage": 58
+      }
+    },
+    {
+      image: "img/weapons/Wst_Shooter_TripleMiddle_01.png",
+      name: "H-3 Nozzlenose D",
+      shotUnit: "shots",
+      sub: "Sprinkler",
+      special: "Ink Armor",
+      level: 30,
+      specialCost: 190,
+      inkSaver: "High",
+      inkPerShot: 1.8,
+      speedLevel: "Middle",
+      price: 18400,
+      stats: {
+        "Range": 70,
+        "Fire Rate": 30,
+        "Damage": 58
+      }
     }
-    ]
+  ]
   },
   {
     type: "Rollers/Brushes",
