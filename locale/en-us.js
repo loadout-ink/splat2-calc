49d2ee4a4ee52730612a9a521b8190a4d0df1eb4
Devi <jschrip@gmail.com>
2017-09-09 07:37:10 -0700

    Add more strings and comment for translators.

--- locale/en-us.js
+++ locale/en-us.js
@@ -1,10 +1,26 @@
+// TRANSLATION INSTRUCTIONS
+// * Don't translate locale variables - just their values
+//// (i.e. "HAT_SLOT": "<translation here>")
+// * Don't translate comments (like these)
+// * Don't translate variables (double bracket words) within phrases ("{{value}} HP")
+//// Move the variable to where it makes sense in the translation.
+// * Try not to make things much longer than necessary
+// * Use in-game terms where possible
+// * "Distance Units"/"Distance Units per Frame" should always be translated the same way
+//// You can call them something different, but make sure it can be abbreviated as a unit!
+//// (Distance Units/frame -> DU/f)
+
 var en_strings = {
+  // Gear section
   "HAT_SLOT": "Head",
   "CLOTHES_SLOT": "Clothes",
   "SHOES_SLOT": "Shoes",
+
+  // Stats section
   "STATS_HEADER": "Stats",
   "INACCURATE_DISCLAIMER": "Stat is inaccurate due to lack of data.",
   "BOMB_HEADER": "Bomb Damage",
+  // Will be used in tables - keep these short
   "BOMB_TABLE_BOMB": "Bomb",
   "TABLE_DIRECT": "Direct",
   "TABLE_NEAR": "Near",
@@ -12,13 +28,19 @@ var en_strings = {
   "TABLE_HIT": "Hit",
   "SPECIAL_HEADER": "Special Damage",
   "SPECIAL_TABLE_SPECIAL": "Special",
+
+  // Footer
   "FOOTER_COPYRIGHT": "Splatoon and all related copyrights/assets used on this page are the property of Nintendo.",
   "FOOTER_ABOUT": "about",
 
+  // Misc. Strings
   "UNAVAILABLE": "Unavailable",
+  "UNRELEASED": "Unreleased",
+  //// Will be used in tables - keep these short
   "KID_FORM": "Kid",
   "SQUID_FORM": "Squid",
 
+  // Popup headers/buttons
   "UI_WEAPON_PICKER": "Weapon Picker",
   "UI_GEAR_PICKER": "Gear Picker",
   "UI_ABOUT": "About",
@@ -27,11 +49,17 @@ var en_strings = {
   "UI_CANCEL": "Cancel",
   "UI_CONFIRM_CASUAL": "Got it!",
 
+  // Tooltips for utility buttons
   "UI_COPY_LINK": "Copy Link",
   "UI_SCREENSHOT_MODE": "Toggle Screenshot Mode",
   "UI_CLEAR_ABILITIES": "Clear Abilities",
   "UI_CLEAR_ALL": "Clear All",
+  // Tooltips for special icons
+  "UI_NONSTANDARD_SPLATNET": "Non-standard. Only on SplatNet",
+  "UI_NOT_POSSIBLE": "Not possible with current main",
 
+  // Stats - these appear over the stat bars.
+  // If there is an asterisk (*), leave it in after the stat name.
   "STAT_SWIM_SPEED": "Swim Speed",
   "STAT_RUN_SPEED": "Run Speed",
   "STAT_RUN_SPEED_RESIST": "Run Speed (Enemy Ink)",
@@ -63,13 +91,16 @@ var en_strings = {
   "STAT_QUICK_RESPAWN_PUNISHER": "Quick Respawn Time *",
   "STAT_TRACKING_TIME": "Tracking Time",
 
+  // Labels that go on the stat bars themselves. Keep these SHORT!
   "LABEL_DISTANCE_PER_FRAME": "{{value}} DU/f",
   "LABEL_PERCENT": "{{value}}%",
+  // LABEL_MAIN_COST is combined with "SHOT_UNIT" values below
   "LABEL_MAIN_COST": "{{value}}% tank/{{unit}}",
   "LABEL_SUB_COST": "{{value}}% tank",
   "LABEL_TIME": "{{value}}s",
   "LABEL_HP": "{{value}} HP",
 
+  // Tooltips for stats
   "DESC_PUNISHER_DISCLAIMER": "Effects with Respawn Punisher aren't fully understood.",
   "DESC_TRACKING": "Point Sensor/Ink Mine duration",
   "DESC_RECOVERY_TIME": "{{value}}s from empty to full",
@@ -78,9 +109,10 @@ var en_strings = {
   "DESC_SPECIAL_COST": "{{value}}p for special",
   "DESC_DISTANCE": "{{value}} Distance Units",
   "DESC_QUICK_RESPAWN": "Respawn time when splatted without splatting others.",
-
   "UNIT_DISTANCE_UNITS_PER_FRAME": "Distance Units/frame",
 
+  // The "unit" weapons fire in
+  // Used in phrases like "200 shots to empty"
   "SHOT_UNIT_SHOT": "shot",
   "SHOT_UNIT_SHOT_PLURAL": "shots",
   "SHOT_UNIT_FLICK": "flick",
@@ -94,14 +126,50 @@ var en_strings = {
   "SHOT_UNIT_CHARGER": "full charge",
   "SHOT_UNIT_CHARGER_PLURAL": "full charges",
 
-  "INK_BREAKDOWN_SHOTS_AFTER_SUB": "{{unit}} after sub: {{value}}",
+  // Future feature - ink breakdown
+  //// Combined with SHOT_UNIT
+  "INK_BREAKDOWN_SHOTS_AFTER_SUB": "{{unitPlural}} after sub: {{value}}",
   "INK_BREAKDOWN_TOTAL_SHOTS": "{{value}} to empty",
   "INK_BREAKDOWN_INK_PER_SHOT": "{{value}}/{{unit}}",
   "INK_BREAKDOWN_INK_PER_SECOND": "{{value}}/second",
   "INK_BREAKDOWN_RECOVERY": "Full Recovery Time",
 
+  // Future feature - Comeback/Tenacity/
   "HEADER_WITH_ABILITY": "{{ability}} Stats",
 
+  // Future feature - a few explanations
+  "UI_DISTANCE_EXPLANATION": "Distance Units per Frame is a measurement of speed in-game.",
+  "UI_DISTANCE_EQUIVALENT": "Each line in the testing area is 50 Distance Units apart.",
+  "UI_FRAMES_EXPLANATION": "One second is 60 frames.",
+
+  // Future feature - options
+  "UI_OPTIONS": "Options",
+  "UI_ENABLE_UNRELEASED": "Enable Unreleased Weapons",
+  "UI_UNRELEASED_DISCLAIMER": "Stats for unreleased weapons may not be accurate.",
+  "UI_UNRELEASED_WEAPONS": "Unreleased Weapons",
+  // Future feature - gear tabs
+  "UI_SPLATNET_GEAR": "SplatNet Gear",
+  "UI_STANDARD_GEAR": "Standard Gear",
+  "UI_UNRELEASED_GEAR": "Unreleased Gear",
+  // Future feature - set saving
+  "UI_LOADOUT": "Loadout",
+  "UI_SAVE_LOADOUT": "Save Loadout",
+  "UI_LOAD_LOADOUT": "Load Loadout",
+  "UI_SAVE": "Save",
+  "UI_LOAD": "Load",
+  "UI_NAME": "Name",
+  "UI_ENTER_NAME": "Enter a name for this loadout:",
+  "UI_ERROR_NAME_TOO_LONG": "Name must be shorter than 20 characters.",
+  "UI_ERROR_DUPLICATE_NAME": "You already have a loadout with this name.",
+  "UI_ERROR_INVALID_NAME": "Please pick a different name.",
+  "UI_DELETE_LOADOUT": "Delete Loadout",
+  "UI_DELETE": "Delete",
+  "UI_VERIFY": "Are you sure?",
+  "UI_SAVED_LOADOUTS": "Saved Loadouts",
+  "UI_DELETE_ALL": "Delete All Loadouts",
+
+  // This goes in the "about" popup.
+  /// Leave the HTML tags alone.
   "UI_ABOUT_CONTENT": `Built primarily with AngularJS 1.6.5 and Bootstrap 3.<br><br>
   This tool was developed using info and assets datamined by <a href="https://twitter.com/LeanYoshi" target="_blank">Lean</a>, as well as information provided by several members of the Inkademy and Splatoon Developers servers on Discord.
   <br><br>Thanks to everyone who gave me help and data!<br><br>
