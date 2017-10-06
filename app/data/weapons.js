604483389ce6f77c07211a1568ccb6e81b351a64
Sam Wong <sam@hellosam.net>
2017-10-06 12:55:21 +0800

    Missing shotUnit for Hero Slosher Replica

--- app/data/weapons.js
+++ app/data/weapons.js
@@ -2209,6 +2209,7 @@ angular.module('splatApp').weapons = function ($scope) {
             "fr_CA": "Seauceur héroïque (rép.)",
             "de_DE": "Helden-Schwapper Replik"
           },
+          shotUnit: "{{ SHOT_UNIT_SLOSHER | translate }}",
           sub: "Suction Bomb",
           special: "Tenta Missiles",
           level: 5,
