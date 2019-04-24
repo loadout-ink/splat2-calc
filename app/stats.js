1bfa6a3aa9c4c52f0f09276678fbc4df682cfc51
Selicia <selicia@gmail.com>
2019-04-24 03:50:02 -0400

    Fixed bug introduced by 65ac6c8 which caused the Ink Saver Main stat title to not reset when switching off a Brush or Roller.

--- app/stats.js
+++ app/stats.js
@@ -277,6 +277,7 @@ angular.module('splatApp').stats = function ($scope) {
       }
       else {
         costPerShot = loadout.weapon.inkPerShot * reduction;
+        this.name = "{{ STAT_SAVER_MAIN | translate }}";
         this.desc = "{{ DESC_MAIN_COST | translate }}".format({totalShots: Math.floor(100/costPerShot), reduction: (100 - (reduction*100)).toFixed(1)});       
         this.label = "{{ LABEL_MAIN_COST | translate }}".format({value: $scope.toFixedTrimmed(costPerShot,3), unit: loadout.weapon.shotUnit});
       }
